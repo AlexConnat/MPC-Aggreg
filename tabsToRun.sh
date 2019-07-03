f7b43ead9d333365e89c5994fd46da8b7d1a4c1c
AlexConnat <alexandre.connat@epfl.ch>
2019-07-03 22:44:09 +0200

    Remember to launch the server (mpc.pid == 0) at last, because HE is the one doing the benchmarking

--- tabsToRun.sh
+++ tabsToRun.sh
@@ -7,10 +7,13 @@ for ((i=1;i<=NB_CLIENTS+1;i++)); do
     repeat_params+="-P localhost "
 done
 
-cmd="python3 server_all_main.py $repeat_params -I 0"
-gnome-terminal --tab -t "Server" -- bash -c "$cmd"
-
 for ((client_id=1;client_id<=NB_CLIENTS;client_id++)); do    
     cmd="python3 server_all_main.py $repeat_params -I $client_id"
     gnome-terminal --tab -t "Client $client_id" -- bash -c "$cmd"
 done
+
+# important to run the server at LAST, because he's the one measuring the
+# computation time:
+cmd="python3 server_all_main.py $repeat_params -I 0"
+gnome-terminal --tab -t "Server" -- bash -c "$cmd"
+
