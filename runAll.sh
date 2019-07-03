5c408d64f29dd31ecb2ac86d342c7a541b5b5951
AlexConnat <alexandre.connat@epfl.ch>
2019-07-03 22:05:39 +0200

    Will now automatically compile the votes #teachers/client, and start the MPC aggregation

--- runAll.sh
+++ runAll.sh
@@ -1,7 +1,16 @@
+
+NB_CLIENTS=$1
+
 if [ -z $1 ]
 then
     echo "Must supply a number of clients!"
     exit
 fi
 
-gnome-terminal --window -- ./tabsToRun.sh $1
+echo "[*] Cleaning up mnist250 folder"
+rm DATA/mnist250/*
+
+echo "[*] Running the votes compilator for $NB_CLIENTS clients (equal split)"
+cd DATA/mnist250/; python3 ../votes_compilator.py ../VOTES_MNIST_250 $NB_CLIENTS; cd ../../;
+
+gnome-terminal --window -- ./tabsToRun.sh $NB_CLIENTS
