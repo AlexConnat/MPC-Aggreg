147f0468f061f0ee34c7e1ef870e3dd52bfeb611
AlexConnat <alexandre.connat@epfl.ch>
2019-07-14 19:42:13 +0200

    Beautiful version of the code. Tag v3.0 from here.

--- main.py
+++ main.py
@@ -1,31 +1,58 @@
 #!/usr/bin/python3
 
+# This piece of code aggregates predictions of machine learning classifiers, trained
+# on MNIST or SVHN datasets (only at the moment). These predictions are compiled as
+# "votes", represented for each sample_id by an vector of K integers, each one representing
+# how many classifiers predicted class "k".
+# E.g, votes_client_0.npy = [2, 0, 13, 5, 0]  means 2 classifiers voted class "0", 13 classifiers
+# voted class "2" and 5 votes class "3". Classes "1" and "4" have no votes.
+
+# This code uses M+1 parties, M "clients" also called "input parties": they own a private
+# input vector containing the classifiers votes. And 1 party that we will call here "server"
+# or "output party", that will receive the final label computed from the aggregated predictions
+# of clients. The server will also choose the client noise values, and is assumed not to be
+# colluding with them.
+
+
+# Standard python imports
+import csv
+import sys
+import time
+
+# Requirements imports
 from mpyc.runtime import mpc
 import numpy as np
-import sys
 
+# Imported from utils.py
 from utils import vector_add_all, scalar_add_all, argmax
 
-import time
-import csv
+
+################################################################################
+
+
+def usage():
+    print(f'Usage: {sys.argv[0]} <mnist|svhn> [nb_samples] [...]')
+    sys.exit()
 
 if len(sys.argv) != 2:
-    print(f'Usage: {sys.argv[0]} <mnist|svhn> [...]')
-    sys.exit()
+    usage()
 
 if sys.argv[1] == 'mnist':
     DATASET = 'mnist250'
 elif sys.argv[1] == 'svhn':
     DATASET = 'svhn250'
 else:
-    print('Error: Should only specify "mnist" or "svhn" as datasets')
-    print('Received:', sys.argv[1])
-    sys.exit()
+    print(f'Error: Invalid dataset "{sys.argv[1]}".\n')
+    usage()
+
+if len(sys.argv) > 2:
+    NB_SAMPLES = sys.argv[2]
 
 SERVER_ID = 0
 TOTAL_NB_TEACHERS = 250
-IS_SERVER = False
 
+# Numbers from the Papernot 2018 paper
+# Total number of samples have been commented out
 if DATASET == 'mnist250':
     #NB_SAMPLES = 10000
     NB_SAMPLES = 640
@@ -41,18 +68,22 @@ elif DATASET == 'svhn250':
     SIGMA1 = 200
     SIGMA2 = 40
 
-
-############### FOR BENCHMARKING PURPOSES ##################
-NB_SAMPLES = 10
-csv_file = open(f'BENCHMARK/{DATASET}_{NB_SAMPLES}s_{len(mpc.parties)-1}c.csv', mode='w')
+# This CSV file will hold the results, each label alongside the time it took to output these labels
+timestamp = int(time.time())
+csv_file = open(f'BENCHMARK/{DATASET}_{len(mpc.parties)-1}c_{NB_SAMPLES}s_{timestamp}.csv', mode='w')
 csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-############################################################
+csv_writer.writerow(['Sample ID','Label','Time elapsed']) # Write CSV Header
+
+# This list will hold the computed labels (or -1 if no label has been given)
+# We store them as 8-bit integers for storage purposes
+LABELS = np.zeros(NB_SAMPLES, dtype=np.int8)
+
+# This numpy file will hold the "LABELS" array in order of sample_id appearance
+numpy_filename = f'labels_{DATASET}_{len(mpc.parties)-1}c_{NB_SAMPLES}s_{timestamp}.npy'
+# --> Actually saved at the end of the code on line "np.save(LABELS, numpy)"
 
 
-
-# Will hold the computed labels (or -1 if label is not defined)
-# -- 8-bit integers type for storage efficiency --
-LABELS = np.zeros(NB_SAMPLES, dtype=np.int8) # 8-bit integers for storage efficiency
+################################################################################
 
 
 # Secure type for integers, and for fixed precision numbers
@@ -63,10 +94,13 @@ secfxp = mpc.SecFxp()
 M = len(mpc.parties)
 print('Number of parties:', M)
 
-# This piece of code is currently running by 'mpc.pid'
+# This piece of code is currently ran by 'mpc.pid'
 my_pid = mpc.pid
 print('my pid:', my_pid)
 
+# The server has party ID 0, every other party is a client
+# Code logic is different for the server and the clients
+IS_SERVER = False
 if my_pid == SERVER_ID:
     IS_SERVER = True
     print('You are the server!')
@@ -74,15 +108,14 @@ if my_pid == SERVER_ID:
 
 # Each client loads the votes from its .npy file and compute
 # its local parameters
-if my_pid != SERVER_ID:
-    np_votes = np.load(f'DATA/{DATASET}/votes_client_{my_pid}.npy')
-    # Compute my local sigma1 and sigma2, a local parts of the total
+if not IS_SERVER:
+    np_votes = np.load(f'DATA/{DATASET}/votes_client_{my_pid}.npy') # For this PoC the path is hardcoded here
+    # Compute my local sigma1 and sigma2, local fraction of the total
     # variance respecitively SIGMA1 and SIGMA2
-    print('my votes (1st sample):', np_votes[0])
+    # The sum of all sigma^2 of all parties is equal to SIGMA^2
     nb_teachers = int(sum(np_votes[0]))
     sigma1 = float( np.sqrt(float(nb_teachers)/float(TOTAL_NB_TEACHERS)) * SIGMA1 )
     sigma2 = float( np.sqrt(float(nb_teachers)/float(TOTAL_NB_TEACHERS)) * SIGMA2 )
-    print('my noisy votes (e.g.):', list( map(float, np_votes[0] + np.random.normal(0, sigma2, NB_CLASSES)) ))
 
 
 ###############################################################################
@@ -91,9 +124,8 @@ mpc.run(mpc.start())   #### START THE MPC ROUNDS OF COMPUTATION ####
 print("="*50,'\n');
 ###############################################################################
 
+# Reset the timer before the actual computation begins
 last_time = time.time()
-elapsed = last_time - mpc.start_time
-csv_writer.writerow(['Sample ID','Label','Time elapsed'])
 
 # Iterate over all samples in the public dataset
 for sample_id in range(NB_SAMPLES):
@@ -104,8 +136,9 @@ for sample_id in range(NB_SAMPLES):
 
         # Convert the votes to secure type
         my_sec_votes = list(map(secfxp, my_votes))
+
     else:
-        # Server is not an input party (it has no votes)
+        # The server is not an input party (it has no votes)
         my_sec_votes = list(map(secfxp, [None]*NB_CLASSES))
 
     # Secret-share the votes from this party with every other party
@@ -113,7 +146,7 @@ for sample_id in range(NB_SAMPLES):
 
     # Aggregate the secure votes from all parties
     total_sec_votes = vector_add_all(all_sec_votes)
-    #print('total_votes:', mpc.run(mpc.output(total_sec_votes)))
+
 
     if not IS_SERVER:
         # Generate two different noise values (from the same distribution)
@@ -132,16 +165,6 @@ for sample_id in range(NB_SAMPLES):
     all_sec_noises0 = mpc.input(sec_noise0, senders=list(range(1,M)))
     all_sec_noises1 = mpc.input(sec_noise1, senders=list(range(1,M)))
 
-    # # ### DEBUG ### convert them back to float
-    # conv_noises0 = mpc.run(mpc.output(all_sec_noises0))
-    # conv_noises1 = mpc.run(mpc.output(all_sec_noises1))
-    #
-    # if not IS_SERVER:
-    #     conv_noise0 = float(conv_noises0[my_pid-1])
-    #     conv_noise1 = float(conv_noises1[my_pid-1])
-    #     print(noise0, conv_noise0, noise0 - conv_noise0)
-    #     csv_writer.writerow([noise0, conv_noise0, noise0-conv_noise0])
-    #     csv_writer.writerow([noise1, conv_noise1, noise1-conv_noise1])
 
     if IS_SERVER:
         # Generate a selection bit for every client
@@ -151,22 +174,19 @@ for sample_id in range(NB_SAMPLES):
         # Only the server draw these random selection bits
         sec_selection_bits = list(map(secfxp, [None]*(M-1)))
 
-    sec_selection_bits = mpc.input(sec_selection_bits, senders=[SERVER_ID])[0] # Only one sender  -->  Only one element (list) in the list
-    #print('selection bits:', mpc.run(mpc.output(sec_selection_bits)))
+    # mpc.input() returns a list of lists of size len(senders). Here only 1 sender (the server)
+    # Hence we take the 0th element of: [[x,x,x]]  =>  [x,x,x] of length the number of clients
+    sec_selection_bits = mpc.input(sec_selection_bits, senders=[SERVER_ID])[0]
 
     sec_chosen_noises = []
     for client_id, selection_bit_for_client in enumerate(sec_selection_bits):
         # If b is 0, we select noise0 from this client
-        # If b is 0, we select noise1 from this client
-        sec_chosen_noise = mpc.if_else(selection_bit_for_client,
-                        all_sec_noises1[client_id], all_sec_noises0[client_id])
+        # If b is 1, we select noise1 from this client
+        sec_chosen_noise = mpc.if_else(selection_bit_for_client, all_sec_noises1[client_id], all_sec_noises0[client_id])
         sec_chosen_noises.append(sec_chosen_noise)
-
-    #print('chosen noises:', mpc.run(mpc.output(sec_chosen_noises)))
 
     # Aggregate the secure noise values from all parties
     total_sec_noise = scalar_add_all(sec_chosen_noises)
-    #print('total noise:', mpc.run(mpc.output(total_sec_noise)))
 
     # Find the secure maximum in the aggregated array of votes
     sec_max = mpc.max(total_sec_votes)
@@ -186,7 +206,7 @@ for sample_id in range(NB_SAMPLES):
 
             elapsed = time.time() - last_time
             # print('time elapsed:', elapsed)
-            csv_writer.writerow([sample_id, 'NULL', elapsed])
+            csv_writer.writerow([sample_id, -1, elapsed])
             last_time = time.time()
 
         continue # Skip to the next sample_id in the for loop
@@ -197,7 +217,8 @@ for sample_id in range(NB_SAMPLES):
     else:
         if not IS_SERVER:
             # Generate two vectors of gaussian noise values
-            # Again, which noise vector would be used is oblivious to the clients
+            # Again, which one of the 2 noise vectors would be used will be decided
+            # by the server, and oblivious to the clients
             noise_vector0 = list(map(float, np.random.normal(0, sigma2, NB_CLASSES)))
             noise_vector1 = list(map(float, np.random.normal(0, sigma2, NB_CLASSES)))
 
@@ -218,19 +239,13 @@ for sample_id in range(NB_SAMPLES):
         sec_chosen_noise_vectors = []
         for client_id, selection_bit_for_client in enumerate(sec_selection_bits):
             # If b is 0, we select noise0 from this client
-            # If b is 0, we select noise1 from this client
-            # FIXME: Condition must be integral!
-            selection_bit_for_client.integral = True
+            # If b is 1, we select noise1 from this client
+            selection_bit_for_client.integral = True # The 1st param in mpc.if_else must be integral
             sec_chosen_noise_vector = mpc.if_else(selection_bit_for_client, all_sec_noise_vector1[client_id], all_sec_noise_vector0[client_id])
             sec_chosen_noise_vectors.append(sec_chosen_noise_vector)
-            #sec_chosen_noise_vectors.append(all_sec_noise_vector1[client_id])
-
-        #print('chosen noise vectors:', mpc.run(mpc.output(sec_chosen_noise_vectors)))
-
 
         # Aggregate the secure noise vectors from all parties
         total_sec_noise = vector_add_all(sec_chosen_noise_vectors)
-        #print('total noise vector:', mpc.run(mpc.output(total_sec_noise)))
 
         # Add this secure total noise vector to the secure total votes vector
         total_sec_noisy_votes = mpc.vector_add(total_sec_votes, total_sec_noise)
@@ -253,25 +268,6 @@ for sample_id in range(NB_SAMPLES):
         continue # To the next sample_id
 
 
-if IS_SERVER:
-    pass
-    # At the end of the computation, store these labels to a .npy file
-    # Also store the computation time in the filename
-
-    #from datetime import datetime
-    #ts = int(datetime.timestamp(datetime.now()))
-    # import time
-    # elapsed = time.time() - mpc.start_time
-    #
-    # np.save(f'./RESULTS/labels_{DATASET}_{M-1}_clients_{elapsed}.npy', LABELS)
-
-
-
-############### FOR BENCHMARKING PURPOSES ##################
-csv_file.close()
-############################################################
-
-
 ###############################################################################
 print('\n'+'='*50)
 mpc.run(mpc.shutdown())    #### END THE MPC ROUNDS OF COMPUTATION ####
@@ -279,6 +275,10 @@ print('='*50)
 ###############################################################################
 
 
-# TODO: Care about performance? SecInt() SecFxp(), use less bits? use server to
-# select b? Skip first round of communication? (total number of teachers?)
-# Do fancy optimizations to the code?
+# At the end of the computation, store these labels to a .npy file
+# if IS_SERVER:
+#     np.save(f'labels_{DATASET}_{len(mpc.parties)-1}c_{NB_SAMPLES}s_{timestamp}.npy', LABELS)
+
+
+# Close the CSV file used for benchmark
+csv_file.close()
